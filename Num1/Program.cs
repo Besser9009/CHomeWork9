f217d84363d61832782da13e8cd478fc1218a494
besser <besser9009@mail.ru>
2023-04-27 17:08:16 +0300

    add .gitignore

---
+++ Num1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Start();
+void Start()
+{
+    Console.WriteLine("Enter number N and M. N > M");
+    int numN = StartNumber("N");
+    int numM = StartNumber("M");
+    NaturalNumber(numN, numM);
+}
+int StartNumber(string NumberName)
+{
+    Console.Write($"Enter your number {NumberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+void NaturalNumber(int numN, int numM)
+{
+    if (numN > numM) NaturalNumber(numN, numM + 1);
+    Console.Write($"{numM} ");
+}
