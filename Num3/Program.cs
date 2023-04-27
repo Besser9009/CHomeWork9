e4d4fb026c2cc43b12f97804b2227209d81e3bc9
besser <besser9009@mail.ru>
2023-04-27 19:56:23 +0300

    add Num3

---
+++ Num3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Start();
+void Start()
+{
+    Console.WriteLine("Enter number N and M. N > M");
+    uint numN = StartNumber("N");
+    uint numM = StartNumber("M");
+    Console.WriteLine(AkcNum(numN, numM));
+}
+uint StartNumber(string NumberName)
+{
+    Console.Write($"Enter your number {NumberName}: ");
+    uint num = Convert.ToUInt32(Console.ReadLine());
+    return num;
+}
+uint AkcNum(uint numN, uint numM)
+{
+    if (numN == 0) return numM + 1;
+    else if ((numN != 0) && (numM == 0)) return AkcNum(numN - 1, 1);
+    else return AkcNum(numN - 1, AkcNum(numN, numM - 1));
+    
+}
